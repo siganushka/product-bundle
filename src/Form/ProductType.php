e30b3aa595e495ed8de70c63d53ff2f6c02ababa
siganushka <siganushka@gmail.com>
2024-01-23 20:11:38 +0800

    refactor: merge product type

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Siganushka\ProductBundle\Form;
 
 use Siganushka\MediaBundle\Form\Type\MediaType;
+use Siganushka\ProductBundle\Entity\Option;
 use Siganushka\ProductBundle\Entity\Product;
-use Siganushka\ProductBundle\Form\EventListener\AddProductOptionsListener;
-use Siganushka\ProductBundle\Form\EventListener\AddProductVariantsListener;
+use Siganushka\ProductBundle\Entity\ProductVariant;
 use Siganushka\ProductBundle\Media\ProductImg;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -31,14 +35,49 @@ class ProductType extends AbstractType
             ])
         ;
 
-        $builder->addEventSubscriber(new AddProductOptionsListener());
-        $builder->addEventSubscriber(new AddProductVariantsListener());
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, [$this, 'onPreSetData']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Product::class,
+        ]);
+    }
+
+    public function onPreSetData(FormEvent $event): void
+    {
+        $data = $event->getData();
+        if (!$data instanceof Product) {
+            return;
+        }
+
+        $form = $event->getForm();
+        $form->add('options', EntityType::class, [
+            'label' => 'product.options',
+            'class' => Option::class,
+            'choice_label' => fn (Option $choice): string => (string) $choice,
+            'disabled' => $data->getId() ? true : false,
+            'multiple' => true,
+            'expanded' => true,
+        ]);
+
+        if (null === $data->getId()) {
+            return;
+        }
+
+        $prototypeData = new ProductVariant();
+        $prototypeData->setProduct($data);
+
+        $form->add('variants', CollectionType::class, [
+            'label' => 'product.variants',
+            'entry_type' => ProductVariantType::class,
+            'entry_options' => ['label' => false],
+            'prototype_data' => $prototypeData,
+            'allow_add' => true,
+            'allow_delete' => true,
+            'error_bubbling' => false,
+            'by_reference' => false,
         ]);
     }
 }
