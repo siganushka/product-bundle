72eb39144dd1a6541f46357c922278df1608f6ed
siganushka <siganushka@gmail.com>
2024-09-21 15:28:34 +0800

    tests: add routes tests case

---
+++ tests/Routing/RoutesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\ProductBundle\Tests\Routing;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Routing\AttributeRouteControllerLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Routing\Loader\AttributeDirectoryLoader;
+use Symfony\Component\Routing\Loader\PhpFileLoader;
+
+class RoutesTest extends TestCase
+{
+    /**
+     * @dataProvider routesProvider
+     */
+    public function testRotues(string $routeName, string $path, array $methods): void
+    {
+        $locator = new FileLocator(__DIR__.'/../../config/');
+
+        new LoaderResolver([
+            $loader = new PhpFileLoader($locator),
+            new AttributeDirectoryLoader($locator, new AttributeRouteControllerLoader()),
+        ]);
+
+        $routes = $loader->load('routes.php');
+
+        static::assertSame($path, $routes->get($routeName)?->getPath());
+        static::assertSame($methods, $routes->get($routeName)?->getMethods());
+    }
+
+    public static function routesProvider(): iterable
+    {
+        yield ['siganushka_product_product_getcollection', '/products', ['GET']];
+        yield ['siganushka_product_product_postcollection', '/products', ['POST']];
+        yield ['siganushka_product_product_getitem', '/products/{id}', ['GET']];
+        yield ['siganushka_product_product_putitem', '/products/{id}', ['PUT', 'PATCH']];
+        yield ['siganushka_product_product_putitemvariants', '/products/{id}/variants', ['PUT', 'PATCH']];
+        yield ['siganushka_product_product_deleteitem', '/products/{id}', ['DELETE']];
+        yield ['siganushka_product_productvariant_getitem', '/product-variants/{id}', ['GET']];
+        yield ['siganushka_product_productvariant_putitem', '/product-variants/{id}', ['PUT', 'PATCH']];
+        yield ['siganushka_product_productvariant_deleteitem', '/product-variants/{id}', ['DELETE']];
+    }
+}
