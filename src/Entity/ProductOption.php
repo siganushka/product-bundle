01ee7ad191d321f5c00eaa1c2152295aaab51ac0
siganushka <siganushka@gmail.com>
2025-07-17 17:01:03 +0800

    refactor: remove name argument for ProductOption constructor

--- src/Entity/ProductOption.php
+++ src/Entity/ProductOption.php
@@ -31,9 +31,8 @@ class ProductOption implements ResourceInterface, TimestampableInterface
     #[ORM\OrderBy(['createdAt' => 'ASC', 'id' => 'ASC'])]
     protected Collection $values;
 
-    public function __construct(?string $name = null)
+    public function __construct()
     {
-        $this->name = $name;
         $this->values = new ArrayCollection();
     }
 
