271bf9c303b6d1738c8eb92daec46805b41cf886
siganushka <siganushka@gmail.com>
2025-09-03 17:24:14 +0800

    refactor: Using attribute route replace annotation route

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,7 +12,7 @@ use Siganushka\ProductBundle\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class ProductController extends AbstractController
--- src/Controller/ProductOptionController.php
+++ src/Controller/ProductOptionController.php
@@ -10,7 +10,7 @@ use Siganushka\ProductBundle\Repository\ProductOptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class ProductOptionController extends AbstractController
