d9be8a78976a4b4f8df16035f13bc123fdda4aee
siganushka <siganushka@gmail.com>
2025-11-26 18:02:44 +0800

    tests: improve test routes

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -10,10 +10,14 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\Routing\Loader\AttributeDirectoryLoader;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 class RoutesTest extends TestCase
 {
-    public function testRotues(): void
+    protected RouteCollection $routes;
+
+    protected function setUp(): void
     {
         $locator = new FileLocator(__DIR__.'/../config/');
 
@@ -22,7 +26,11 @@ class RoutesTest extends TestCase
             new AttributeDirectoryLoader($locator, new AttributeRouteControllerLoader()),
         ]);
 
-        $routes = $loader->load('routes.php');
+        $this->routes = $loader->load('routes.php');
+    }
+
+    public function testAll(): void
+    {
         static::assertSame([
             'siganushka_product_product_getcollection',
             'siganushka_product_product_postcollection',
@@ -33,6 +41,32 @@ class RoutesTest extends TestCase
             'siganushka_product_product_putvariants',
             'siganushka_product_productoption_getitem',
             'siganushka_product_productoption_putitem',
-        ], array_keys($routes->all()));
+        ], array_keys($this->routes->all()));
+    }
+
+    /**
+     * @dataProvider routesProvider
+     */
+    public function testRotues(string $routeName, string $path, array $methods): void
+    {
+        /** @var Route */
+        $route = $this->routes->get($routeName);
+
+        static::assertSame($path, $route->getPath());
+        static::assertSame($methods, $route->getMethods());
+        static::assertTrue($route->getDefault('_stateless'));
+    }
+
+    public static function routesProvider(): iterable
+    {
+        yield ['siganushka_product_product_getcollection', '/products', ['GET']];
+        yield ['siganushka_product_product_postcollection', '/products', ['POST']];
+        yield ['siganushka_product_product_getitem', '/products/{id}', ['GET']];
+        yield ['siganushka_product_product_putitem', '/products/{id}', ['PUT', 'PATCH']];
+        yield ['siganushka_product_product_deleteitem', '/products/{id}', ['DELETE']];
+        yield ['siganushka_product_product_getvariants', '/products/{id}/variants', ['GET']];
+        yield ['siganushka_product_product_putvariants', '/products/{id}/variants', ['PUT', 'PATCH']];
+        yield ['siganushka_product_productoption_getitem', '/product-options/{id}', ['GET']];
+        yield ['siganushka_product_productoption_putitem', '/product-options/{id}', ['PUT', 'PATCH']];
     }
 }
